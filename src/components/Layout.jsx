584d0eb02f4d3ad754886d5d7b8335ef747fc844
Renier Vargas <renier1989@gmail.com>
2023-09-13 06:15:01 -0500

    Class157. Creando el Layout Principal

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,10 +2,14 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div>
-      <nav className="text-5xl font-bold text-gray-600">This is the nav</nav>
-      {/* esto me permite pasarle los hijos definidos en el createBrowserRouter  */}
-      <Outlet />
+    <div className="md:flex md:min-h-screen">
+        <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+            <h2 className="text-4xl font-black text-center text-white">CRM - Clients</h2>
+        </aside>
+
+        <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
+            <Outlet/>
+        </main>
     </div>
   );
 }
