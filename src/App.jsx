6893259aa3f051633545912e9bf9e38152264174
Renier Vargas <renier1989@gmail.com>
2023-09-13 05:49:33 -0500

    Class155. Creando el Router y definiendo primeras rutas

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <div className="bg-red-400 text-white text-3xl">hola mundo</div>
-    </>
-  );
-}
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Hello here's where the fun begins</h1>
+  },
+{
+  path:'/tags',
+  element: <h1>Tag page</h1>
+}
+]);
+
+// const router = createHashRouter([
+//   {
+//     path: '/',
+//     element: <h1>Hello here's where the fun begins</h1>
+//   },
+//   {
+//     path:'/tags',
+//     element: <h1>Tag page</h1>
+//   }
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
