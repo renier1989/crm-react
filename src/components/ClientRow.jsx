75bb5f972705f0458a84ebdd7739a26ed123a827
Renier Vargas <renier1989@gmail.com>
2023-09-14 09:03:09 -0500

    Class162 - iterando los clientes y estilos en la tabla

---
+++ src/components/ClientRow.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+function ClientRow({ client }) {
+  const { nombre, empresa, email, telefono, id } = client;
+  return (
+    <tr className="border-b">
+      <td className="p-5 space-y-2">
+        <p className="text-2xl text-gray-800">{nombre}</p>
+        <p>{empresa}</p>
+      </td>
+      <td className="p-5 space-y-2">
+        <p className=" text-gray-600"> <span className="text-gray-900 uppercase font-bold">Email: </span> {email}</p>
+        <p className=" text-gray-600"> <span className="text-gray-900 uppercase font-bold">Phone: </span> {telefono}</p>
+      </td>
+      <td className="p-5 flex space-x-3">
+        <button className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs">Edit</button>
+        <button className="text-red-600 hover:text-red-700 uppercase font-bold text-xs">Delete</button>
+      </td>
+    </tr>
+  );
+}
+
+export default ClientRow;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,60 +1,78 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from "react-router-dom";
+import ClientRow from "../components/ClientRow";
 
-
-export function loader(){
-
+export function loader() {
   const clients = [
     {
-        id: 1,
-        nombre: 'Juan',
-        telefono: 102013313,
-        email: "juan@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 1,
+      nombre: "Juan",
+      telefono: 102013313,
+      email: "juan@juan.com",
+      empresa: "Codigo Con Juan",
     },
     {
-        id: 2,
-        nombre: 'Karen',
-        telefono: 138198313,
-        email: "karen@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 2,
+      nombre: "Karen",
+      telefono: 138198313,
+      email: "karen@juan.com",
+      empresa: "Codigo Con Juan",
     },
     {
-        id: 3,
-        nombre: 'Josue',
-        telefono: 31983913,
-        email: "josue@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 3,
+      nombre: "Josue",
+      telefono: 31983913,
+      email: "josue@juan.com",
+      empresa: "Codigo Con Juan",
     },
     {
-        id: 4,
-        nombre: 'Miguel',
-        telefono: 319381983,
-        email: "miguel@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 4,
+      nombre: "Miguel",
+      telefono: 319381983,
+      email: "miguel@juan.com",
+      empresa: "Codigo Con Juan",
     },
     {
-        id: 5,
-        nombre: 'Pedro',
-        telefono: 1398198938,
-        email: "pedro@juan.com",
-        empresa: 'Codigo Con Juan'
+      id: 5,
+      nombre: "Pedro",
+      telefono: 1398198938,
+      email: "pedro@juan.com",
+      empresa: "Codigo Con Juan",
     },
-];
+  ];
 
   return clients;
 }
 
 function Index() {
-
   const data_clients = useLoaderData();
-  console.log("🚀 ~ file: index.jsx:11 ~ Index ~ data:", data_clients)
-
   return (
     <>
-    <h1 className="font-black text-4xl text-blue-900">Clients</h1>
-    <p className="mt-3">Manage your Clients</p>
+      <h1 className="font-black text-4xl text-blue-900">Clients</h1>
+      <p className="mt-3">Manage your Clients</p>
+
+      {data_clients.length > 0 ? (
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Client</th>
+              <th className="p-2">Contact</th>
+              <th className="p-2">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data_clients.map(client => (
+              <ClientRow
+                client={client}
+                key={client.id}
+              />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No Clients registered.</p>
+      )}
     </>
-  )
+  );
 }
 
-export default Index
+export default Index;
