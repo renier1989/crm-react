f296b66ef4edb4c994323754ccec1394c478e187
Renier Vargas <renier1989@gmail.com>
2023-09-14 09:23:55 -0500

    Class164. Primeros Pasos creando el Formulario de Clientes

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,8 +1,31 @@
+import { useNavigate } from "react-router-dom";
 
 function NewClient() {
+  const navigate = useNavigate();
   return (
-    <div>NewClient</div>
-  )
+    <>
+      <div className="flex justify-between mb-10 items-center">
+        <div>
+          <h1 className="font-black text-4xl text-blue-900">New Clients</h1>
+          <p className="mt-3">
+            Fill all the information to register a new client
+          </p>
+        </div>
+        <div>
+          <button
+            className="bg-blue-800 text-white px-3 py-1 font-bold uppercase hover:bg-blue-700 hover:animate-pulse"
+            onClick={() => navigate("/")}
+          >
+            Go Back
+          </button>
+        </div>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
+        <p>Here will go the form</p>
+      </div>
+    </>
+  );
 }
 
-export default NewClient
+export default NewClient;
