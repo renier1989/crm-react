cbd0122bddba3d65d0ea6bc18104bd32bbf312ed
Renier Vargas <renier1989@gmail.com>
2023-09-14 12:55:54 -0500

    Class171 - Implementando json-server para similar un REST-API

---
+++ src/data/clients.jsx
@@ -0,0 +1,8 @@
+export async function getClients(){
+    const URL = import.meta.env.VITE_API_URL;
+    const reponse = await fetch(URL);
+    const result = await reponse.json();
+
+    console.log(result);
+    return result;
+}
